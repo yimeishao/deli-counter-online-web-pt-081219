4d7632f030ab208231db38ff1ac4dc1ef88eba25
Maria Yimei Shao <maria.yimei.shao@gmail.com>
2019-08-26 00:57:49 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,9 +3,4 @@ def line(array)
   if array.size == 0 
    return "The line is currently empty."
  else 
-   while array != 0 
-   array.each do |name, index| 
-     puts "The line is currently: #{index}. #{name}"
-     index += 1 
-  end
-end
+   
