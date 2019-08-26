26396a3134241d74e87ecfda400545007bb2f4f2
Maria Yimei Shao <maria.yimei.shao@gmail.com>
2019-08-26 01:14:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -19,7 +19,7 @@ def now_serving(array)
   if array.length == 0 
     puts "There is nobody waiting to be served!"
   else 
-    puts array[0]
+    puts "Currently serving #{array[0]}."
     array.shift
   end
 end
